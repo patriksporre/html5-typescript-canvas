d0fd8f47e61c7956516ceb0f58a089e49298a705
Patrik Sporre <patriksporre@gmail.com>
2024-11-30 21:43:31 +0100

    Added randomRange

--- source/engine/utils/helper.ts
+++ source/engine/utils/helper.ts
@@ -9,3 +9,14 @@
 export function clamp(value: number, min: number, max: number): number {
     return Math.min(max, Math.max(min, value));
 }
+
+/**
+ * Generates a random number within a specified range.
+ *
+ * @param {number} min - The minimum value of the range (inclusive)
+ * @param {number} max - The maximum value of the range (exclusive)
+ * @returns {number} A random number between min (inclusive) and max (exclusive)
+ */
+export function randomRange(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+}
