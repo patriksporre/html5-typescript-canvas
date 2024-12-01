ac270ef126bbf4660b3aa34efb40d9c6d117ac0a
Patrik Sporre <patriksporre@gmail.com>
2024-12-01 14:15:34 +0100

    00template | [replace] (copy and make magic)

---
+++ source/examples/00template/effect.ts
@@ -0,0 +1,26 @@
+/**
+ * Project: html5-typescript-canvas
+ * File: effect.ts
+ * Author: Patrik Sporre
+ * License: MIT
+ * Description:
+ *   [replace]
+ */
+
+import { Blitter } from "../../engine/blitter.js";              // Blitter class for managing canvas operations
+
+/**
+ * Initializes the effect. Logs a message to the console.
+ */
+export function initialize() {
+    console.log('00template | [replace]');
+}
+
+/**
+ * [replace]
+ * 
+ * @param {Blitter} blitter - An instance of the Blitter class for canvas operations
+ * @param {number} elapsedTime - The total elapsed time since the effect started, in seconds
+ */
+export function render(blitter: Blitter, elapsedTime: number) {
+}
